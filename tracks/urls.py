314abef32e5c7881412dacb07283fb44a86370ae
Andrew <andrewdcar@gmail.com>
2019-06-30 21:39:55 +0100

    added soutes/ slug

--- tracks/urls.py
+++ tracks/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from routes.views import home_page
+from routes.views import routes_page
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page, name='home'),
+    path('routes/', routes_page, name='routes'),
 ]
