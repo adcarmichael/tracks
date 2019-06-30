e52fc4f9c93bc289906ff04539e8f7791db76eee
Andrew <andrewdcar@gmail.com>
2019-06-30 21:39:43 +0100

    Crteate route view mock

--- routes/views.py
+++ routes/views.py
@@ -2,7 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from django.views.generic.base import TemplateView
+from routes import services
+
+dal = services.get_dal()
 
 
 def home_page(request):
     return render(request, 'home_page.html')
+
+
+def add_route_set(request, details):
+    pass
+
+
+def routes_page(request):
+
+    data = {
+        'purple': dal.get_route_set_of_colour('purple'),
+        'orange': dal.get_route_set_of_colour('orange'),
+        'green': dal.get_route_set_of_colour('green'),
+        'yellow': dal.get_route_set_of_colour('yellow'),
+        'blue': dal.get_route_set_of_colour('blue'),
+        'white': dal.get_route_set_of_colour('white'),
+        'red': dal.get_route_set_of_colour('red'),
+        'black': dal.get_route_set_of_colour('black')
+    }
+
+    return render(request, 'routes.html', data)
