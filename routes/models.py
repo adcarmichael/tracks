768a5488b8e3345fa0c4a6789d37e4b919017f56
Andrew <andrewdcar@gmail.com>
2019-08-31 06:55:08 +0100

    adds date field ti riyterecord

--- routes/models.py
+++ routes/models.py
@@ -58,6 +58,7 @@ def update_user_profile(sender, instance, created, **kwargs):
 class RouteRecord(Model):
     route = ForeignKey(Route, on_delete=CASCADE)
     user = ForeignKey(Profile, on_delete=CASCADE)
+    up_date = DateField(null=False)
     status = IntegerField(
         verbose_name='E.g. mastered, climbed, attempted, todo')
     is_climbed = models.BooleanField(default=False)
