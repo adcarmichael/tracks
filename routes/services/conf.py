484218da521b90264e923a2e8ff1b8a346b05d10
Andrew <andrewdcar@gmail.com>
2019-09-10 21:54:01 +0100

    moved icons to conf to keep out of view

--- routes/services/conf.py
+++ routes/services/conf.py
@@ -15,6 +15,20 @@ class GymKey(_Enum):
     eden_rock_carlisle = 'eden_carl'
 
 
+def forDjango(cls):
+    cls.do_not_call_in_templates = True
+    return cls
+
+
+@forDjango
+class GradeSubIcon(_Enum):
+    lowest = 'fa fa-arrow-circle-down fa-2x'
+    low = 'fa fa-arrow-circle-right fa-2x rotate-45-right'
+    medium = 'fa fa-arrow-circle-right fa-2x'
+    high = 'fa fa-arrow-circle-up fa-2x rotate-45-right'
+    highest = 'fa fa-arrow-circle-up fa-2x'
+
+
 class Grade(_Enum):
     purple = 1
     orange = 2
@@ -46,3 +60,11 @@ class ClimbStatus(_Enum):
     def get_default():
         self = ClimbStatus
         return self.unclimbed
+
+
+def get_grade_names():
+    return [e.name for e in Grade]
+
+
+def get_grade_sub_names():
+    return [e.name for e in GradeSub]
