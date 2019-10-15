d7963140d650f25216c6b0a705dfba7315d764e4
Andrew <andrewdcar@gmail.com>
2019-10-15 12:18:37 +0100

    Includes python-decouple - missed from last commit

--- src/tracks/settings.py
+++ src/tracks/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,22 +19,30 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-
-if 'DJANGO_DEBUG_FALSE' in os.environ:
+is_production = config('DJANGO_DEBUG_FALSE', default=False, cast=bool)
+if is_production:
     DEBUG = False
-    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = [os.environ['SITENAME']]
+    SECRET_KEY = config('DJANGO_SECRET_KEY')
+    ALLOWED_HOSTS = [config('SITENAME')]
+    EMAIL_USE_TLS = True
+    EMAIL_HOST = 'smtp.sendgrid.net'
+    EMAIL_HOST_USER = 'apikey'
+    EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
+    EMAIL_PORT = '587'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 else:
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY = 'insecure-key-for-dev'
     ALLOWED_HOSTS = ['*']
+    EMAIL_USE_TLS = True
+    EMAIL_HOST = 'smtp.mailtrap.io'
+    EMAIL_HOST_USER = 'ff422522572a0f'
+    EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD_TRAP')
+    EMAIL_PORT = '2525'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.mailtrap.io'
-EMAIL_HOST_USER = 'ff422522572a0f'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-EMAIL_PORT = '2525'
 
+# breakpoint()
 # Application definition
 
 INSTALLED_APPS = [
