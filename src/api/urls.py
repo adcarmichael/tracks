1887bc7d176d0010e9a039bb7eadadefa4a5bb6d
Andrew Carmichael <andrewdcar@gmail.com>
2019-12-30 20:33:52 +0000

    added name temp

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from api.views import ChartData
 
-urlpatterns=[
-   path('chart/data',ChartData.as_view()),
+urlpatterns = [
+    path('chart/data', ChartData.as_view(), name='chart_data'),
 ]
